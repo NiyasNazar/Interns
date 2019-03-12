b768122e4d9e46a86d020fd06d5d5bd886bc3030
Niyas <niyasnazar23@gmail.com>
2019-03-12 17:51:34 +0530

    initialcommit

--- app/src/main/java/remind/edu/myapplication/Login.java
+++ app/src/main/java/remind/edu/myapplication/Login.java
@@ -1,9 +1,11 @@
 package remind.edu.myapplication;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Login extends AppCompatActivity {
@@ -26,5 +28,24 @@ Button button_otp,btn_get_started;
         btn_get_started=(Button)findViewById(R.id.get_started);
        button_otp.setTypeface(buttonfont);
 btn_get_started.setTypeface(buttonfont);
+
+
+button_otp.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+
+    }
+});
+btn_get_started.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent get_started=new Intent(getApplicationContext(),Register.class);
+        startActivity(get_started);
+
+    }
+});
+
+
+
     }
 }
--- app/src/main/java/remind/edu/myapplication/Register.java
+++ app/src/main/java/remind/edu/myapplication/Register.java
@@ -1,20 +1,40 @@
 package remind.edu.myapplication;
 
 import android.graphics.Typeface;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
-    TextView tvstarted;
+    TextView tvstarted,tvsubmit;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        TextInputLayout usernameTextObj = (TextInputLayout)    findViewById(R.id.l1);
+        TextInputLayout  mobiletObj = (TextInputLayout)    findViewById(R.id.l2);
+        TextInputLayout   educationObj = (TextInputLayout)    findViewById(R.id.l3);
+        TextInputLayout   addTextObj = (TextInputLayout)    findViewById(R.id.l4);
+        TextInputLayout   emailTextObj = (TextInputLayout)    findViewById(R.id.l5);
+        Typeface   hintfont = Typeface.createFromAsset(getAssets(), "fonts/Melbourne_reg.otf");
+
+        usernameTextObj .setTypeface(hintfont);
+        mobiletObj .setTypeface(hintfont);
+        educationObj .setTypeface(hintfont);
+        addTextObj .setTypeface(hintfont);
+        emailTextObj .setTypeface(hintfont);
+
         Typeface buttonfont = Typeface.createFromAsset(getAssets(), "fonts/DIN_Alternate_Bold.ttf");
+
+
+
+
         tvstarted=(TextView)findViewById(R.id.tv_getstarted);
+        tvsubmit=(TextView)findViewById(R.id.tv_submit);
         tvstarted.setTypeface(buttonfont);
+        tvsubmit.setTypeface(buttonfont);
     }
 }
